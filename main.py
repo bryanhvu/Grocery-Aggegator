defaf0137340eb3acf1b5deefb7ac75ea5446253
bryanhvu <bryanhvu5@gmail.com>
2022-04-12 13:20:41 -0500

    Setup basic project requirements

---
+++ main.py
@@ -0,0 +1,17 @@
+#TODO Implement web scraper to retrieve product pricing from specified retailer websites
+
+#TODO Setup SQLite and PostgreSQL database with SQLAlchemy to store products
+
+#TODO Design REST API to handle adding, deleting, and updating product database
+
+#TODO Display basic webpages with product data
+
+#TODO Include tag metadata in database to allow filtering/sorting products in webpage
+
+#TODO Run webscraper daily to routinely update product pricing
+
+#TODO Compile comprehensive list of relevant products in database
+
+#TODO Beautify HTML and CSS
+
+#TODO Deploy website with Heroku
